22f352a8a83300dc16986f8b162da7d5566be91d
Stijn <6910589+njitsh@users.noreply.github.com>
2022-12-10 19:26:00 +0100

    Day 10

---
+++ Days/Day10.java
@@ -0,0 +1,98 @@
+package Days;
+import java.util.*;
+
+public class Day10 extends Day {
+    public static void main(String[] args) {
+        System.out.println(new Day10().part1());
+        System.out.println(new Day10().part2());
+    }
+
+    public List<String> getInput(String location) {
+        return super.getInput(location);
+    }
+
+    public String part1() {
+        List<String> input = getInput("Days/input_day10.txt");
+
+        int x = 1;
+        int cycles = 0;
+        int i = 0;
+
+        String instruction = input.get(i);
+
+        int signalStrength = 0;
+
+        while (i < input.size()) {
+
+            if ((cycles - 20) % 40 == 0) {
+                signalStrength += cycles * x;
+                System.out.println("Signal strength: " + cycles * x);
+                System.out.println("Cycles: " + cycles);
+                System.out.println("X: " + x);
+                System.out.println();
+            }
+
+            if (instruction.substring(0,4).equals("addx")) {
+                int newX = x + Integer.parseInt(instruction.substring(5));
+                instruction = "setx " + newX;
+            } else if (instruction.substring(0,4).equals("setx")) {
+                x = Integer.parseInt(instruction.substring(5));
+                instruction = "noop";
+            }
+
+            cycles++;
+
+            if (!instruction.substring(0,4).equals("setx")) {
+                i++;
+                if (i < input.size()) instruction = input.get(i);
+            }
+        }
+
+        return Integer.toString(signalStrength);
+    }
+
+    public String part2() {
+        List<String> input = getInput("Days/input_day10.txt");
+
+        int x = 1;
+        int cycles = 0;
+        int i = 0;
+
+        String instruction = input.get(i);
+
+        String crt = "";
+
+        while (i < input.size()) {
+            if ((cycles - 20) % 40 == 0) {
+                System.out.println("Signal strength: " + cycles * x);
+                System.out.println("Cycles: " + cycles);
+                System.out.println("X: " + x);
+                System.out.println();
+            }
+            
+            if (instruction.substring(0,4).equals("addx")) {
+                int newX = x + Integer.parseInt(instruction.substring(5));
+                instruction = "setx " + newX;
+            } else if (instruction.substring(0,4).equals("setx")) {
+                x = Integer.parseInt(instruction.substring(5));
+                instruction = "noop";
+            }
+
+            cycles++;
+
+            if (!instruction.substring(0,4).equals("setx")) {
+                i++;
+                if (i < input.size()) instruction = input.get(i);
+            }
+
+            if (Math.abs((cycles % 40) - x) <= 1) crt += "X";
+            else crt += ".";
+
+            if (cycles % 40 == 0) {
+                crt += "\n";
+            }
+        }
+
+        return crt;
+    }
+}
